90b0739c1987a3128b9664a852f9d56604e93113
ParitoshChaudhari <chaudhariparitosh9571@gmail.com>
2024-12-24 18:52:26 +0530

    prod minor changes

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var methodOverride = require('method-override')
 const {v4:uuid4} = require("uuid");
 
 app.set("view engine","ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname,"public")));
 
 app.use(express.urlencoded({extended:true}));
